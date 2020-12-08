860d36a824753f765d89dbe63fe11cbdb3b076b0
U-GLOBAL\D037559 <daniel.ritter@sap.com>
2020-12-08 09:21:14 +0100

    added some more tests

--- pipeline/src/main/java/com/orientechnologies/orient/pipeline/Importer.java
+++ pipeline/src/main/java/com/orientechnologies/orient/pipeline/Importer.java
@@ -5,6 +5,7 @@ import org.apache.commons.math3.distribution.TDistribution;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,12 +16,16 @@ public class Importer {
   public static void main(String[] args) {
     final Importer importer = new Importer();
     final List<String> paths =
-        Arrays.asList("databases_2_2\\Empty.json", "databases_2_2\\OrderCustomer-sl-0.json");
+        Arrays.asList(
+            "databases_2_2\\Empty.json",
+            "databases_2_2\\OrderCustomer-sl-0.json",
+            "databases_3_1\\OrderCustomer-sl-0.json");
     importer.execute(paths, numberOfIterations);
   }
 
   public void execute(final List<String> paths, final int numberOfIterations) {
     try (final ProgressBar pb = new ProgressBar("Iterations", numberOfIterations)) {
+      final List<Workload> workloads = new ArrayList<>();
       for (final String path : paths) {
         final SummaryStatistics importStats = new SummaryStatistics();
         final SummaryStatistics exportStats = new SummaryStatistics();
